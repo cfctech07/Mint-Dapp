e09a82a2855aaabdd292cd0a8f396078fee7ade9
CFC Tech <cfctech07@gmail.com>
2023-12-06 14:38:39 +0530

    app-hardhat-added
    app-hardhat-added

---
+++ hardhat-files/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x314277AAAe235392a831CfaB9da1A754c5d02d09";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "http://nft-collection-d3vd00m.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
